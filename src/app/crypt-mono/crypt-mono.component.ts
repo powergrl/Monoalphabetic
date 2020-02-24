2398c42066c1ddec1545c6dd78d76a216cace248
Hyunzee Lee <hylee@ucsd.edu>
2020-02-24 00:32:40 -0800

    Created an array for the alphabets and displayed them

--- src/app/crypt-mono/crypt-mono.component.ts
+++ src/app/crypt-mono/crypt-mono.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CryptMonoComponent implements OnInit {
 
+  alphabet;
+
   constructor() { }
 
   ngOnInit() {
+    this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   }
 
 }
