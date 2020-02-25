7059f923bf702238efb569cedbca5b7ac410c719
Hyunzee Lee <hylee@ucsd.edu>
2020-02-25 14:13:03 -0800

    Added radio buttons and array

--- src/app/crypt-mono/crypt-mono.component.ts
+++ src/app/crypt-mono/crypt-mono.component.ts
@@ -7,12 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CryptMonoComponent implements OnInit {
 
-  alphabet;
+  plainText;
+  cypherText;
 
   constructor() { }
 
   ngOnInit() {
-    this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    this.plainText = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+                      "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+                      "W", "X", "Y", "Z"];
+    this.cypherText = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                       "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                       "w", "x", "y", "z"];
   }
 
 }
