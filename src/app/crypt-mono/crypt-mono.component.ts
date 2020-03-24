884a6bcb3a04f9e2760433fda9d77a2bae625c28
Hyunzee Lee <hylee@ucsd.edu>
2020-03-24 15:53:57 -0700

    Fixed up style and added letters

--- src/app/crypt-mono/crypt-mono.component.ts
+++ src/app/crypt-mono/crypt-mono.component.ts
@@ -5,20 +5,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crypt-mono.component.html',
   styleUrls: ['./crypt-mono.component.css']
 })
+
 export class CryptMonoComponent implements OnInit {
 
-  plainText;
-  cypherText;
+  plainText: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+                    "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+                    "W", "X", "Y", "Z"];
+  cypherText: string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                     "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                     "w", "x", "y", "z"];
+  blank: string[] = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+                " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ", " ",
+                " ", " "];
 
   constructor() { }
 
   ngOnInit() {
-    this.plainText = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
-                      "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-                      "W", "X", "Y", "Z"];
-    this.cypherText = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
-                       "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
-                       "w", "x", "y", "z"];
+
   }
 
 }
